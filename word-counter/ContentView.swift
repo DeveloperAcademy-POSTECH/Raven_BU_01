4f165739ea0802d1f30a9cb3f98db6d34c7fa930
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-29 12:15:28 +0900

    Init: build project

---
+++ word-counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  word-counter
+//
+//  Created by apple developer academy on 2022/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ word-counter/word_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  word_counterApp.swift
+//  word-counter
+//
+//  Created by apple developer academy on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct word_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
